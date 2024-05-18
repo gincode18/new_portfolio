bac5c66e5cc6d71a75e8785c6e8dd3f2b50af2e6
gincode18 <vishalkamboj9211@gmail.com>
2024-05-18 13:37:11 +0530

    Initial Template :)

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -15,14 +15,11 @@ export const postRouter = createTRPCRouter({
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
       return ctx.db.post.create({
-        data: {
-          title: "user1@prisma.io",
-        },
+        data: {},
       });
     }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.user.findMany({
-    });
+    return ctx.db.user.findMany({});
   }),
 });
