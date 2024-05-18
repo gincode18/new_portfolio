1d42273942ff2e9ca39dbb5b8876452e78f065c5
gincode18 <vishalkamboj9211@gmail.com>
2024-05-18 12:55:31 +0530

    Initial Template :)

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.post.create({
+        data: {
+          title: "user1@prisma.io",
+        },
+      });
+    }),
+
+  getLatest: publicProcedure.query(({ ctx }) => {
+    return ctx.db.user.findMany({
+    });
+  }),
+});
