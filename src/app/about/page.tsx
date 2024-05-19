e467f0ed54ca1c26dadc163555dd2de16a72d22b
gincode18 <vishalkamboj9211@gmail.com>
2024-05-19 17:51:34 +0530

    Done with About :)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,9 +13,9 @@ const AboutPage = () => {
     Time: string;
     Company: string;
   };
-  const exprerience: Experience[] = [
+  const EXPERIENCE: Experience[] = [
     {
-      Position: "Associate - Middleware Engineer",
+      Position: "Associate Middleware Engineer",
       Company: "Credain",
       Desciption:
         "Led the development and maintenance of middleware solutions using Node.js, enhancing integration and communication ",
@@ -229,7 +229,7 @@ const AboutPage = () => {
               initial={{ x: "-300px" }}
               animate={isExperienceRefInView ? { x: "0" } : {}}
               transition={{ delay: 0.2 }}
-              className="text-2xl font-bold"
+              className="text-2xl font-bold text-primary"
             >
               EXPERIENCE
             </motion.h1>
@@ -239,101 +239,39 @@ const AboutPage = () => {
               animate={isExperienceRefInView ? { x: "0" } : {}}
               className=""
             >
-              {/* EXPERIENCE LIST ITEM */}
-              <div className="flex h-48 justify-between">
-                {/* LEFT */}
-                <div className="w-1/3 ">
-                  {/* JOB TITLE */}
-                  <div className="rounded-b-lg rounded-s-lg bg-white p-3 font-semibold">
-                    Senior JavaScript Engineer
+              {EXPERIENCE.map((EXPERIENCE): any => {
+                return (
+                  <div className="flex justify-between gap-6">
+                    {/* LEFT */}
+                    <div className=" flex flex-col  justify-between items-center ">
+                      {/* JOB TITLE */}
+                      <div className="rounded-b-lg rounded-s-lg bg-secondary p-3 font-semibold">
+                        {EXPERIENCE.Position}
+                      </div>
+                      {/* JOB DESC */}
+                      <div className="p-3 text-sm italic">
+                        {EXPERIENCE.Desciption}{" "}
+                      </div>
+                      {/* JOB DATE */}
+                      <div className=" text-sm font-semibold text-red-400">
+                        {EXPERIENCE.Time}
+                      </div>
+                      {/* JOB COMPANY */}
+                      <div className=" rounded bg-secondary p-1 mb-3 text-sm font-semibold">
+                        {EXPERIENCE.Company}
+                      </div>
+                    </div>
+                    <div className="flex w-1/6 justify-center">
+                      {/* LINE */}
+                      <div className="relative h-full w-1 rounded bg-accent">
+                        {/* LINE CIRCLE */}
+                        <div className="absolute -left-2 h-5 w-5 rounded-full bg-secondary ring-4 ring-primary"></div>
+                      </div>
+                    </div>
+               
                   </div>
-                  {/* JOB DESC */}
-                  <div className="p-3 text-sm italic">
-                    I led web development, offering expertise in JavaScript
-                    frameworks.{" "}
-                  </div>
-                  {/* JOB DATE */}
-                  <div className="p-3 text-sm font-semibold text-red-400">
-                    2024 - Present
-                  </div>
-                  {/* JOB COMPANY */}
-                  <div className="w-fit rounded bg-white p-1 text-sm font-semibold">
-                    Apple
-                  </div>
-                </div>
-                {/* CENTER */}
-                <div className="flex w-1/6 justify-center">
-                  {/* LINE */}
-                  <div className="relative h-full w-1 rounded bg-gray-600">
-                    {/* LINE CIRCLE */}
-                    <div className="absolute -left-2 h-5 w-5 rounded-full bg-white ring-4 ring-red-400"></div>
-                  </div>
-                </div>
-                {/* RIGHT */}
-                <div className="w-1/3 "></div>
-              </div>
-              {/* EXPERIENCE LIST ITEM */}
-              <div className="flex h-48 justify-between">
-                {/* LEFT */}
-                <div className="w-1/3 "></div>
-                {/* CENTER */}
-                <div className="flex w-1/6 justify-center">
-                  {/* LINE */}
-                  <div className="relative h-full w-1 rounded bg-gray-600">
-                    {/* LINE CIRCLE */}
-                    <div className="absolute -left-2 h-5 w-5 rounded-full bg-white ring-4 ring-red-400"></div>
-                  </div>
-                </div>
-                {/* RIGHT */}
-                <div className="w-1/3 ">
-                  {/* JOB TITLE */}
-                  <div className="rounded-b-lg rounded-s-lg bg-white p-3 font-semibold">
-                    Senior React Developer
-                  </div>
-                  {/* JOB DESC */}
-                  <div className="p-3 text-sm italic">
-                    I spearheaded React-based application development,
-                    leveraging advanced skills.{" "}
-                  </div>
-                  {/* JOB DATE */}
-                  <div className="p-3 text-sm font-semibold text-red-400">
-                    2019 - 2024{" "}
-                  </div>
-                  {/* JOB COMPANY */}
-                  <div className="w-fit rounded bg-white p-1 text-sm font-semibold">
-                    Microsoft
-                  </div>
-                </div>
-              </div>
-              {/* EXPERIENCE LIST ITEM */}
-              <div className="flex h-48 justify-between">
-                {/* LEFT */}
-                <div className="w-1/3 ">
-                  {/* JOB TITLE */}
-                  <div className="rounded-b-lg rounded-s-lg bg-white p-3 font-semibold">
-                    Freelancer{" "}
-                  </div>
-                  {/* JOB DESC */}
-                  <div className="p-3 text-sm italic">
-                    I provided web solutions, applying a range of technologies
-                    to address client requirements.{" "}
-                  </div>
-                  {/* JOB DATE */}
-                  <div className="p-3 text-sm font-semibold text-red-400">
-                    2010 - 2019{" "}
-                  </div>
-                </div>
-                {/* CENTER */}
-                <div className="flex w-1/6 justify-center">
-                  {/* LINE */}
-                  <div className="relative h-full w-1 rounded bg-gray-600">
-                    {/* LINE CIRCLE */}
-                    <div className="absolute -left-2 h-5 w-5 rounded-full bg-white ring-4 ring-red-400"></div>
-                  </div>
-                </div>
-                {/* RIGHT */}
-                <div className="w-1/3 "></div>
-              </div>
+                );
+              })}
             </motion.div>
           </div>
         </div>
