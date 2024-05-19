c75a782c81b8be632a07733b29ee0b31adad8170
gincode18 <vishalkamboj9211@gmail.com>
2024-05-19 23:00:21 +0530

    Fixed Projectjs :)

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    domains: ["images.unsplash.com",'images.pexels.com'],
+    domains: ["images.unsplash.com",'images.pexels.com','res.cloudinary.com'],
   },
 };
 
