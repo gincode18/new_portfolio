335753abfdf7215809707b86eb0faa08b9c451eb
gincode18 <vishalkamboj9211@gmail.com>
2024-05-20 17:14:00 +0530

    Some Minor fix :)

--- src/app/_components/Home.tsx
+++ src/app/_components/Home.tsx
@@ -5,6 +5,7 @@ import hero from "../../../public/profile.jpeg";
 import { motion } from "framer-motion";
 import React from "react";
 import ThreeDCard from "./3DCARD";
+import Link from "next/link";
 
 const Homepage = () => {
   return (
@@ -37,12 +38,16 @@ const Homepage = () => {
           </p>
           {/* BUTTONS */}
           <div className="flex w-full gap-4">
-            <button className="rounded-lg bg-primary p-4 text-secondary ring-2 ring-accent duration-700 ease-out  hover:bg-accent">
-              View My Work
-            </button>
-            <button className="rounded-lg p-4 ring-2 ring-primary">
-              Contact Me
-            </button>
+            <Link href="/portfolio">
+              <button className="rounded-lg bg-primary p-4 text-secondary ring-2 ring-accent duration-700 ease-out  hover:bg-accent">
+                View My Work
+              </button>
+            </Link>
+            <Link href="/contact">
+              <button className="rounded-lg p-4 ring-2 ring-primary">
+                Contact Me
+              </button>
+            </Link>
           </div>
         </div>
       </div>
